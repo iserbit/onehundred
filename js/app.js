09e80514389918330429ce23a4f162f996309d6d
iserbit <kevin.kik.lee@gmail.com>
2016-07-07 23:16:09 +0000

    Display Quote

--- js/app.js
+++ js/app.js
@@ -8,16 +8,28 @@
     var textdata = this; 
     textdata.text = []; 
     this.paragraph = [];
+    this.quote = []; 
     this.disp=false; 
     
 	  $http.get('/data/onehundred.json').success(function(data){
 		  textdata.text = data;
 	  });
     
-    
+    this.cleartext = function() {
+      this.disp=false; 
+      var quotedata=this; 
+      var quoteindex=0; 
+      quotedata.quote=[]; 
+      this.quote=[]; 
+      quoteindex=Math.floor(10*Math.random());
+      
+      
+      $http.get('/data/onehundred-quotes.json').success(function(data){
+  		  quotedata.quote = data.quote[quoteindex];
+  	  });
+    }
     
     this.selection = function(num) {
-        this.disp=false; 
         this.paragraph = [];
         var array=[]; 
         
@@ -28,11 +40,34 @@
         }
         this.paragraph=array;
         this.disp=true; 
-        
-        console.log(index); 
+
     };
+    
+    
+    var quotedata=this; 
+    var quoteindex=0; 
+    quotedata.quote=[]; 
+    this.quote=[]; 
+    quoteindex=Math.floor(10*Math.random());
+    
+    
+    $http.get('/data/onehundred-quotes.json').success(function(data){
+		  quotedata.quote = data.quote[quoteindex];
+	  });
+	  
+	
+    console.log(this.quote);
+    
+    this.quote=quotedata.quote;
+    
+    
 
   });
+  
+  app.controller("QuoteController", ["$http", function($http){
+
+    
+  }]);
   
   
 })();
